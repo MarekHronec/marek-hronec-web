ab99ebe59a0143183089acaf9b5189fdd04669bd
Marek Hronec <hronec.marek.fir@gmail.com>
2026-04-07 20:07:49 +0200

    fix: add base path for GitHub Pages project deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marekhronec.github.io',
+  base: '/marek-hronec-web',
   integrations: [sitemap()],
   output: 'static',
 });
